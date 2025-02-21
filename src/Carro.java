7bd8a14be7acf098cca2c5687780286f255b7610
GuilherrmeSouza <guionesouza@gmail.com>
2025-02-20 21:43:41 -0300

    Exercício modelo de OO em Java

---
+++ src/Carro.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+public class Carro {
+    // variaveis
+    // vamos trabalhar com tipo de dados primitivos (letra minuscula) int, float
+    public int ano;
+    public float velocidade;
+    //tipo de dado é uma classe (letra maiuscula) String
+    public String marca, modelo;
+
+    //Metodo construtores
+    //Sem parametro
+
+    public Carro(){
+        this.marca = "Vazia";
+        this.modelo = "Vazio";
+    }
+    public Carro(int ano, float velocidade, String marca, String modelo){
+        this.ano = ano;
+        this.velocidade = velocidade;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    //Metodos
+    public void exibirDetalhes(){
+        //this representa o objeto que se chama "Metodo"
+        JOptionPane.showMessageDialog(null, "Marca: " + this.marca +  " ; Modelo: " + this.modelo + " ; Velocidade " + this.velocidade);
+    }
+    public void acelerar(float x ){
+        this.velocidade = this.velocidade + x;
+    }
+    public void frear(float x){
+        if(this.velocidade - x >= 0){
+            this.velocidade -= x;
+        }
+        else{
+            System.out.println("Frenagem não ocorreu !" );
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        //criando objetos da classe Carro
+        // = instanciar a classe Carro
+        Carro obj1 = new Carro();
+        obj1.ano = 2004;
+        obj1.velocidade = 0.0f;
+        obj1.marca = "VM";
+        obj1.modelo = "Fox";
+        obj1.exibirDetalhes();
+        obj1.acelerar(50);
+        obj1.exibirDetalhes();
+        obj1.frear(51);
+        obj1.exibirDetalhes();
+        obj1.acelerar(60);
+        obj1.exibirDetalhes();
+
+        Carro obj2 = new Carro(2008, 0.0f, "Honda", "Civic");
+        obj2.exibirDetalhes();
+
+        Carro obj3 = new Carro(2010, 0.0f, "GM", "Astra");
+        obj3.exibirDetalhes();
+
+        Carro obj4 = new Carro();
+        obj4.exibirDetalhes();
+    }
+
+}
